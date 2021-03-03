401d073a0920794e0c4989c597b315d4fc3d633f
Torstein Olsen <torstein@knett.no>
2021-03-04 00:32:57 +0100

    Fiksa timer til Controller.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,12 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 public class Controller {
 
@@ -16,25 +18,34 @@ public class Controller {
     @FXML
     void initialize() {
         clicker = new Main();
+        updateGUI();
     }
 
     @FXML
     void countAction() {
         clicker.bitcoinOnClick();
-        counterOutput.setText("Antall bitcoins: " + clicker.getTotalBitcoins());
+        counterOutput.setText(String.format("Antall bitcoins: %.0f", clicker.getTotalBitcoins()));
     }
 
-//    public void timerScreenUpdate() {
-//        Timer timerGUI = new Timer();
-//        TimerTask updateRate = new TimerTask() {
-//            @Override
-//            public void run() {
-//                counterOutput.setText("Antall bitcoins: " + clicker.getTotalBitcoins());
-//            }
-//        };
-//
-//        timerGUI.schedule(updateRate, 0, 20);
-//    }
+    @FXML
+    void updateGUI() {
+        Timer timer = new Timer();
+        TimerTask updateRate = new TimerTask() {
+            @Override
+            public void run() {
+                //runLater to be able to use Timers in Controller
+                Platform.runLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        counterOutput.setText(String.format("Antall bitcoins: %.0f", clicker.getTotalBitcoins()));
+                    }
+                });
+            }
+        };
+
+        timer.schedule(updateRate, 0, 20);
+    }
 
 }
 
